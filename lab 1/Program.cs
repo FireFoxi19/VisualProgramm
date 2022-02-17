c74fb86d7ae93c1c6c8968a8972e269b234a0d8f
Татьяна <@->
2022-02-17 13:32:19 +0700

    Добавьте файлы проекта.

---
+++ lab 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums2 = new int[4] { 10, 2, 3, 3 };
+            Console.WriteLine(Kata.QueueTime(nums2, 2));
+        }
+    }
+
+
+    public class Kata
+    {
+        public static long QueueTime(int[] customers, int n)
+        {
+
+            if (customers.Length == 0) { return 0; } 
+            if (n == 1) { return customers.Sum(); } 
+            if (n >= customers.Length) { return customers.Max(); } 
+
+            int[] checkout = new int[n];
+
+            foreach (int customer in customers)
+            {
+                Array.Sort(checkout);
+                checkout[0] += customer;
+            }
+
+            return checkout.Max();
+        }
+    }
+}
